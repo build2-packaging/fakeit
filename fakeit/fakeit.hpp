cb28ad7d2dfe9a47592b151ec14f915fa53dfa77
helmesjo <helmesjo@gmail.com>
2022-08-25 16:00:52 +0200

    build2: Setup lib{fakeit} & tests ('basics' test tests importation of fakeit lib, and 'unit' are the tests pulled in from the fakeit repo ('/upstream') which never imports the lib (since they require a special fakeit.hpp file etc). Latter is useful to verify supported platforms/compilers.

--- fakeit/fakeit.hpp
+++ fakeit/fakeit.hpp
@@ -1,15 +1,1 @@
-#pragma once
-
-#include <iosfwd>
-#include <string>
-
-#include <fakeit/export.hpp>
-
-namespace fakeit
-{
-  // Print a greeting for the specified name into the specified
-  // stream. Throw std::invalid_argument if the name is empty.
-  //
-  FAKEIT_SYMEXPORT void
-  say_hello (std::ostream&, const std::string& name);
-}
+../upstream/single_header/standalone/fakeit.hpp
--- tests/basics/driver.cpp
+++ tests/basics/driver.cpp
@@ -1,35 +1,34 @@
-#include <sstream>
-#include <stdexcept>
+#include <iostream>
+#include <string>
 
 #include <fakeit/version.hpp>
+// Support both include forms (for backwards compatibility)
 #include <fakeit/fakeit.hpp>
+#include <fakeit.hpp>
 
-#undef NDEBUG
-#include <cassert>
+using namespace std;
+
+struct some_interface
+{
+  virtual int foo(int) = 0;
+  virtual int bar(string) = 0;
+};
 
 int main ()
 {
-  using namespace std;
   using namespace fakeit;
 
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
+  // Instantiate a mock object.
+  Mock<some_interface> mock;
+  
+  // Setup mock behavior.
+  When(Method(mock,foo)).Return(1); // Method mock.foo will return 1 once.
+  
+  // Fetch the mock instance.
+  some_interface &i = mock.get();
+  
+  // Will print "1". 
+  cout << i.foo(0);
 
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
+  return 0;
 }
